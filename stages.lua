c9e9a0d98d1602689d847efe93dc7f22e49f0918
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2022-12-14 01:11:57 +0200

    fix stage14 preview

--- stages.lua
+++ stages.lua
@@ -186,7 +186,9 @@ xxxx
 ]]
 
 function stage14()
-    while t-spawn_t>=33 do
+    local speedup=33
+    if love.update==menu_update then speedup=0 end
+    while t-spawn_t>=speedup do
         local i=1
         local tx,ty=0,0
         local a=spawn_t*0.4+pi/2
@@ -202,7 +204,7 @@ function stage14()
             if char=='x' then
                 for i=0,3-1 do
                 local a2=a+i*2*pi/3
-                ins(bullets,{x=320/2-cos(a2)*20+cos(a2)*ty*5+cos(a2-pi/2)*tx*5,y=200/2-40-sin(a2)*20+sin(a2)*ty*5+sin(a2-pi/2)*tx*5,dx=cos(a2)*2.5,dy=sin(a2)*2.5,bt=spawn_t+33})
+                ins(bullets,{x=320/2-cos(a2)*20+cos(a2)*ty*5+cos(a2-pi/2)*tx*5,y=200/2-40-sin(a2)*20+sin(a2)*ty*5+sin(a2-pi/2)*tx*5,dx=cos(a2)*2.5,dy=sin(a2)*2.5,bt=spawn_t+speedup})
                 end
             end
             tx=tx+1
