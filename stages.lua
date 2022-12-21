829cef008b9a3eb40b91221473ca3ea0b1112fe0
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2022-12-21 20:07:01 +0200

    revamp stage23

--- stages.lua
+++ stages.lua
@@ -421,12 +421,12 @@ function stage22()
 end
 
 function stage23()
-    acc=acc or t*0.9
+    acc=acc or t*28.9
     while t-spawn_t>=1 do
-        ins(bullets,{x=320/2,y=200/2-40,dx=cos(acc*0.4)*0.2*8.8,dy=sin(acc*0.4)*0.2*8.8,bt=spawn_t+1})
+        ins(bullets,{x=320/2,y=200/2-40,dx=cos(acc*0.12)*0.2*8.8,dy=sin(acc*0.12)*0.2*8.8,bt=spawn_t+1})
         spawn_t=spawn_t+1
     end
-    acc=acc+t*0.01
+    acc=acc+t*0.03
 end
 
 function stage24()
